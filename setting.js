8024e4bd13d9782129f6364b097b7ab6a37bde10
tokjin <41167277+tokjin@users.noreply.github.com>
2020-06-02 18:38:54 +0900

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,27 @@
+let getParam = (name, url) => {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+    let results = regex.exec(url);
+    if(!results) return null;
+    if(!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+const currentVer = 'v1.0.0';
+
+// 文字の大きさを変更
+const fontSize = getParam('fontSize') || 0;
+
+// 未確定の文字を表示しない
+const pendingNone = getParam('pendingNone') || false;
+
+// 文字の色(未確定)
+const pendingTextColor = getParam('pendingTextColor') || false;
+
+// 文字の色(確定済み)
+const finalTextColor = getParam('finalTextColor') || false;
+
+// 縦書きモード
+const verticalMode = getParam('tategaki') || false;
+
