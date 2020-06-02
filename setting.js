bcfc3a57e419536421d9f73e12aeda062cc0b352
tokjin <41167277+tokjin@users.noreply.github.com>
2020-06-03 07:25:45 +0900

    bugfix
    stroneNoneで縁が消えないバグを修正

--- setting.js
+++ setting.js
@@ -8,7 +8,7 @@ let getParam = (name, url) => {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-const currentVer = 'v1.0.7';
+const currentVer = 'v1.0.8';
 
 // 文字の大きさを変更
 const fontSize = getParam('fontSize') || 51;
